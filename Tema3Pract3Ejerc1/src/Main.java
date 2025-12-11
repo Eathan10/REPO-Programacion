2ff0a5eb5b81f16461833e89e745d3301a1a3afa
Eathan <eathanandres.garzon.51296@ikasle.egibide.org>
2025-12-11 10:11:53 +0100

    Estructura definida

--- Tema3Pract3Ejerc1/src/Main.java
+++ Tema3Pract3Ejerc1/src/Main.java
@@ -1,15 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    ArrayList<String> porteros = new ArrayList<>();
+    ArrayList<String> defensas = new ArrayList<>();
+    ArrayList<String> delanteros = new ArrayList<>();
+    ArrayList<String> centrocampistas = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        try {
+
+            mostrarMenu();
+
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+
+
+
+    }
+
+    public static void mostrarMenu() throws Exception{
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Con que grupo quieres trabajar: \n" +
+                                                                    "1. Porteros\n" +
+                                                                    "2. Defensas\n" +
+                                                                    "3. Delanteros\n" +
+                                                                    "4. Centrocampistas"));
+        switch(opcion){
+            case 1:
+                porterosOperaciones();
+                break;
+            case 2:
+                defensasOperaciones();
+                break;
+            case 3:
+                delanterosOperaciones();
+                break;
+            case 4:
+                centroCampistasOperaciones();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"Opcion no valida");
+        }
+
+    }
+
+    public static void porterosOperaciones() throws Exception{
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("¿Que operacion quieres hacer? \n" +
+                                                                    "1. Dar de alta\n" +
+                                                                    "2. Dar de baja\n" +
+                                                                    "3. Modificar\n"));
+        switch(opcion){
+            case 1:
+                porterosOperaciones();
+                break;
+            case 2:
+                defensasOperaciones();
+                break;
+            case 3:
+                delanterosOperaciones();
+                break;
+            case 4:
+                centroCampistasOperaciones();
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"Opcion no valida");
         }
     }
+    public static void defensasOperaciones() throws Exception{}
+    public static void delanterosOperaciones() throws Exception{}
+    public static void centroCampistasOperaciones() throws Exception{}
+
+
 }
