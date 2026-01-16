b806135a7bccf5a8877f126f7654f5ba0b54bfbf
Eathan <eathanandres.garzon.51296@ikasle.egibide.org>
2026-01-16 13:12:48 +0100

    Ejercicio 2 de la practica 2 terminado

--- Tema 4/Tema4Practica2Ejercici2/src/Main.java
+++ Tema 4/Tema4Practica2Ejercici2/src/Main.java
@@ -4,6 +4,7 @@ import Modelo.Pelicula;
 import javax.swing.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
@@ -25,47 +26,82 @@ public class Main {
 
     }
 
-    public static void llenarDatos(){
+    public static void llenarDatos() {
 
-        Estudio estudio1 = new Estudio("Disney","California","Patata 4izquierdo","www.disney.com", LocalDate.of(1886,2,10),"Estados Unidos","123456789");
-        listaEstudios.add(estudio1);
-        Estudio estudio2 = new Estudio("DreamWorks","Nueva York","Oron 4C","www.DeamWorks.com", LocalDate.of(1950,2,10),"Estados Unidos","987654321");
-        listaEstudios.add(estudio2);
-        Estudio estudio3 = new Estudio("Warner","Texas","Lola 8A","www.Warner.com", LocalDate.of(1901,2,10),"Estados Unidos","182453671");
-        listaEstudios.add(estudio3);
+        Estudio disney = new Estudio("Disney", "California", "Patata 4izquierdo", "www.disney.com", LocalDate.of(1923, 10, 16), "Estados Unidos", "123456789");
+        Estudio dreamworks = new Estudio("DreamWorks", "Nueva York", "Oron 4C", "www.DeamWorks.com", LocalDate.of(1994, 10, 12), "Estados Unidos", "987654321");
+        Estudio warner = new Estudio("Warner", "Texas", "Lola 8A", "www.Warner.com", LocalDate.of(1923, 4, 4), "Estados Unidos", "182453671");
 
-
-        ArrayList<Estudio> estudiosP1 = new ArrayList<>();
-        estudiosP1.add(estudio1);
-        estudiosP1.add(estudio2);
-        Pelicula peli1 = new Pelicula("Titanic",1999,160,"Romance",estudiosP1);
-        listaPeliculas.add(peli1);
-        ArrayList<Pelicula> peliculas1 = new ArrayList<>();
-        peliculas1.add(peli1);
-        estudio1.setPeliculas(peliculas1);
+        listaEstudios.add(disney);
+        listaEstudios.add(dreamworks);
+        listaEstudios.add(warner);
 
 
-        ArrayList<Estudio> estidiosP2 = new ArrayList<>();
-        estidiosP2.add(estudio3);
-        Pelicula peli2 = new Pelicula("Los 7 pecados",2006,138,"Aventura",estidiosP2);
-        listaPeliculas.add(peli2);
-        ArrayList<Pelicula> peliculas2 = new ArrayList<>();
-        peliculas2.add(peli2);
-        estudio1.setPeliculas(peliculas2);
+        Pelicula titanic = new Pelicula("Titanic", 1999, 160, "Romance", new ArrayList<>(Arrays.asList(disney, dreamworks)));
+
+        Pelicula pecados = new Pelicula("Los 7 pecados", 2006, 138, "Aventura", new ArrayList<>(Arrays.asList(warner)));
+
+        Pelicula minions = new Pelicula("Minions", 2020, 140, "Animado", new ArrayList<>(Arrays.asList(warner)));
 
 
-        ArrayList<Estudio> estidiosP3 = new ArrayList<>();
-        estidiosP3.add(estudio3);
-        Pelicula peli3 = new Pelicula("Minions",2020,140,"Animado",estidiosP3);
-        listaPeliculas.add(peli3);
+        Pelicula shrek2 = new Pelicula("Shrek 2", 2015, 148, "Animado", new ArrayList<>(Arrays.asList(disney, dreamworks, warner)));
+
+        listaPeliculas.add(titanic);
+        listaPeliculas.add(pecados);
+        listaPeliculas.add(minions);
+        listaPeliculas.add(shrek2);
 
 
-        ArrayList<Estudio> estidiosP4 = new ArrayList<>();
-        estidiosP4.add(estudio1);
-        estidiosP4.add(estudio2);
-        estidiosP4.add(estudio3);
-        Pelicula peli4 = new Pelicula("Shrek 2",2015,148,"Animado",estidiosP4);
-        listaPeliculas.add(peli4);
+        for (Pelicula p : listaPeliculas) {
+            for (Estudio e : p.getListaEstudios()) {
+                if (e.getPeliculas() == null) {
+                    e.setPeliculas(new ArrayList<>());
+                }
+                e.getPeliculas().add(p);
+            }
+        }
+    }
+
+
+    public static void conseguirEstudioConPeliMasLarga(){
+
+        int tiempoMasAlto = 0;
+
+        Pelicula peliMasLarga = new Pelicula();
+        ArrayList<Estudio> estudiosConPeliMasLarga = new ArrayList<>();
+
+        for (Pelicula peli : listaPeliculas){
+            if (peli.getDuracionMinutos() > tiempoMasAlto){
+                tiempoMasAlto = peli.getDuracionMinutos();
+                peliMasLarga = peli;
+                estudiosConPeliMasLarga.addAll(peli.getListaEstudios());
+            }
+        }
+
+        StringBuilder stringConEstudios = new StringBuilder("\n");
+
+        for (Estudio estudio : estudiosConPeliMasLarga){
+            stringConEstudios.append(estudio.getNombre()).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null,"El o los estudios que tienen la peli mas larga es o son: "
+                + stringConEstudios + " con la peli " + peliMasLarga.getTitulo());
+    }
+
+    public static void conseguirEstudioConMasPelis(){
+
+        Estudio estudioConMasPelis = new Estudio();
+
+        int numeroPelisMasAlto = 0;
+
+        for (Estudio estudio : listaEstudios){
+            if (estudio.getPeliculas().size() > numeroPelisMasAlto){
+                numeroPelisMasAlto = estudio.getPeliculas().size();
+                estudioConMasPelis = estudio;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null,"Estudio con mas pelis producidas es: " + estudioConMasPelis.getNombre());
 
     }
 
